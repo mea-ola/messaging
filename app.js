74714ad0e4a728c5b1bac3d95c5d358edf9d8edd
Michael Timbrook <timbrook480@gmail.com>
2015-08-19 16:43:48 -0700

    Initial project setup

---
+++ app.js
@@ -0,0 +1,31 @@
+var app   = require('http').createServer(handler),
+    io    = require('socket.io')(app),
+    redis = require('socket.io-redis'),
+    debug = require('debug')('messaging:server')
+;
+
+io.adapter(redis({ host: 'localhost', port: 6379 }));
+
+var port = process.env.PORT || 3000;
+app.listen(port, () => {
+  debug("Server listening on port " + port);
+});
+
+function handler (req, res) {
+  res.writeHead(200);
+  res.end();
+}
+
+io.on('connection', socket => {
+  debug("Client add " + socket.id);
+  io.emit('message', { data: 'User Connected ' + socket.id });
+  
+  socket.on('message', data => {
+    debug(data);
+    io.emit('message', {
+      from: socket.id,
+      data  
+    });
+  });
+  
+});
